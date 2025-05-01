72243733e75d156f88b9c5f8a9176603cd6c38c3
Fei' Kanthorn <kanthorn9jun@gmail.com>
2025-05-01 14:17:50 +0700

    server setup

--- app.mjs
+++ app.mjs
@@ -7,8 +7,6 @@ import authRouter from "./apps/auth.mjs";
 import profileRouter from "./apps/profileRouter.mjs";
 
 const app = express();
-const port = process.env.PORT || 4001;
-
 app.use(cors());
 app.use(express.json());
 
@@ -21,6 +19,5 @@ app.use("/categories", categoryRouter);
 app.use("/auth", authRouter);
 app.use("/profile", profileRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
-});
+// 👇 Export for Vercel
+export default app;
