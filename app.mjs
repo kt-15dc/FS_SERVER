1a4bf395c6751fa335a9e9a0d3a890c5b6fc6c73
Fei' Kanthorn <kanthorn9jun@gmail.com>
2025-05-01 14:21:14 +0700

    server setup

--- app.mjs
+++ app.mjs
@@ -1,4 +1,6 @@
-import "dotenv/config";
+if (process.env.NODE_ENV !== "production") {
+    await import("dotenv/config");
+  }  
 import express from "express";
 import cors from "cors";
 import postRouter from "./apps/postRouter.mjs";
