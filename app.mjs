356978527c70723a4d128818fd9bed86d9e70caa
Fei' Kanthorn <kanthorn9jun@gmail.com>
2025-05-01 14:12:09 +0700

    server setup

--- app.mjs
+++ app.mjs
@@ -1,5 +1,10 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import postRouter from "./apps/postRouter.mjs";
+import categoryRouter from "./apps/categoryRouter.mjs";
+import authRouter from "./apps/auth.mjs";
+import profileRouter from "./apps/profileRouter.mjs";
 
 const app = express();
 const port = process.env.PORT || 4001;
@@ -10,6 +15,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello TechUp!");
 });
+
+app.use("/posts", postRouter);
+app.use("/categories", categoryRouter);
+app.use("/auth", authRouter);
+app.use("/profile", profileRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
